773a79fde148858a984630e05b1de14bc41927eb
Laurent PELLET <laurentp@ispf.pf>
2022-11-30 20:29:47 -1000

    day 1

--- 2022/laurent-2022-01.R
+++ 2022/laurent-2022-01.R
@@ -3,6 +3,10 @@ name <- "laurent"
 year <- 2022
 day <- 1
 
-
 input <- aoc_get_inputfile(day, year, name)
 
+inputs <- unlist(str_split(input, "\n\n"))
+listInputs <- lapply(inputs, FUN=function(x) unlist(str_split(x,"\n")))
+sumlist <- lapply(listInputs, FUN=function(x) sum(as.integer(x[[1]])))
+indiceMax <- which.max(sumlist)
+sumlist[indiceMax]
