78fe2e84c7998b166d250eed833ab592cc1d15d6
Laurent PELLET <laurentp@ispf.pf>
2022-12-01 14:36:52 -1000

    sapply -> better

--- 2022/laurent-2022-01.R
+++ 2022/laurent-2022-01.R
@@ -6,12 +6,11 @@ day <- 1
 input <- aoc_get_inputfile(day, year, name)
 
 inputs <- unlist(str_split(input, "\n\n"))
-listInputs <- lapply(inputs, FUN=function(x) unlist(str_split(x,"\n")))
-sumlist <- unlist(lapply(listInputs, FUN=function(x) sum(as.integer(unlist(x)))))
+listInputs <- lapply(inputs, FUN=function(x) as.integer(unlist(str_split(x,"\n"))))
+sumlist <- sapply(listInputs, sum, na.rm=TRUE)
 sorted_sumlist <- sort(sumlist,decreasing=TRUE)
 
 #reponse 1
 sorted_sumlist[1]
 #reponse 2
 sum(sorted_sumlist[1:3])
-
