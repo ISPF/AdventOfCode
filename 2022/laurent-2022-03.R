f36873d9190a50807b017180ed019423fd5e3b94
Laurent PELLET <laurentP@ispf.local>
2022-12-03 07:59:12 -1000

    Day 3

---
+++ 2022/laurent-2022-03.R
@@ -0,0 +1,47 @@
+source("src/functions.R")
+name <- "laurent"
+year <- 2022
+day <- 3
+
+input <- aoc_get_inputfile(day, year, name)
+
+dt <- data.table(input=unlist(str_split(input, "\n")))
+dt <- dt[input!=""]
+dt[,compartment1:=substring(input, 1, nchar(input)/2)]
+dt[,compartment2:=substring(input, nchar(input)/2+1, nchar(input))]
+
+getIntersect <- function(a, b){
+  intersect(strsplit(a, "")[[1]],
+                 strsplit(b, "")[[1]])
+}
+for(i in 1:nrow(dt))
+  dt[i, inters:=getIntersect(as.character(dt[i,compartment1]),
+                             as.character(dt[i,compartment2]))]
+
+points <- c(1:52)
+names(points) <- c(tolower(LETTERS), LETTERS)
+
+dt[,pts:=points[inters]]
+dt[, sum(pts)]
+
+# Section 2 ---------------------------------------------------------------
+
+dt[, grp:=(.I+2) %/% 3]
+
+getIntersect3 <- function(a, b, c){
+  intersect(
+    intersect(strsplit(a, "")[[1]],
+              strsplit(b, "")[[1]]),
+    strsplit(c, "")[[1]])
+}
+
+res <- lapply(1:100, FUN=function(x) {
+  dt2 <- dt[grp==x]
+  a <- as.character(dt2[1, input])
+  b <- as.character(dt2[2, input])
+  c <- as.character(dt2[3, input])
+  getIntersect3(a,b,c)
+})
+
+res <- unlist(res)
+sum(points[res])
