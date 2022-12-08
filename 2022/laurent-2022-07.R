078b9eb64aa6a79c2ab2a919ff9a9234b4b28026
Laurent PELLET <laurentP@ispf.local>
2022-12-07 18:06:43 -1000

    day 7

---
+++ 2022/laurent-2022-07.R
@@ -0,0 +1,84 @@
+source("src/functions.R")
+library(zoo)
+
+input <- read_lines(aoc_get_inputfile(7, 2022, "laurent"))
+dt <- data.table(input=input)
+dt[, isCmd:=ifelse(input %like% "\\$", TRUE, FALSE)]
+dt[input %like% "\\$ ls", cmd:="ls"]
+dt[input %like% "\\$ cd", cmd:="cd"]
+dt[input %like% "dir ", cmd:="infodir"]
+dt[input %like% "\\$ cd", folder:=tstrsplit(input, " ")[3]]
+dt[is.na(cmd), c("size", "file"):=tstrsplit(input, " ")]
+dt[is.na(cmd), cmd:="infofile"]
+dt[, size:=as.integer(size)]
+dt[,folder:=na.locf(folder)]
+
+i <- 2
+pwd <- "" 
+tree <- list("/"=list()) 
+while(i<length(input)){
+  cmd <- dt[i, cmd]
+  folder <- dt[i, folder]
+  if(cmd=="cd"){ #If cd change pwd and increment i by 1
+    #pwd <- paste(pwd,gsub("\\$ cd ","",input[i]),sep="/")
+    pwd <- paste(pwd,folder,sep="/")
+    if(folder=="..") pwd <- gsub("/[^/]+/\\.\\.$","",pwd)
+    #if(grepl("\\.\\.$",pwd)) pwd <- gsub("/[^/]+/\\.\\.$","",pwd)
+    i <- i+1
+  }
+  if(cmd=="ls"){ #if ls grab content and increment by length of content
+    j <- grep("^\\$",input[(i+1):length(input)])[1] #check where next command is
+    #j <- dt[seq(i+1, .N)][(isCmd)][1, input]
+    if(is.na(j)){ #if we arrived at the end of the input
+      content <- input[(i+1):length(input)]
+      i <- length(input)
+    }else{ #if not
+      content <- input[(i+1):(i+j-1)]
+      i <- i+j
+    }
+    expr <- "list(" #We'll use metaprogramming here
+    for(k in seq_along(content)){
+      if(grepl("^dir ",content[k])){
+        name <- gsub("dir ","",content[k])
+        expr <- paste0(expr,name,"=list(),") #if directory make a new sublist
+      }else if(grepl("^[0-9]",content[k])){
+        expr <- paste0(expr,gsub("[^0-9]","",content[k]),",") #if file just keep size of file
+      }
+    }
+    expr <- paste0(gsub(",$","",expr),")")
+    d <- eval(parse(text=expr)) #content as a sublist
+    path <- el(strsplit(pwd,"/")) #now we figure out where to put it based on current path
+    path <- path[path!=""]
+    expr<-"tree[[1]]" #Our root
+    for(k in seq_along(path)){
+      expr <- paste0(expr,"$",path[k])
+    }
+    expr <- paste0(expr,"<-d")
+    eval(parse(text=expr)) #Place sublist in correct list
+  }
+}
+
+foo <- function(x){ #Recursive function to compute size of list and all its sublists
+  s <- sum(unlist(x))
+  w <- which(sapply(x,class)=="list")
+  if(length(w)){
+    S <- lapply(x[w],foo)
+  }else{
+    S <- 0 #if doesn't content more lists then 0
+  }
+  return(list(s,S))
+}
+allsizes <- foo(tree) #Sizes of all directories
+allsizes <- unlist(allsizes)
+sum(allsizes[allsizes<100000])
+#1444896
+
+target<-30000000-(70000000-allsizes[1]) #Extra space needed
+sort(allsizes[allsizes>=target])[1]
+#404395
+
+
+##Visualization
+library(data.tree)
+dt <- FromListSimple(tree)
+plot(dt)
